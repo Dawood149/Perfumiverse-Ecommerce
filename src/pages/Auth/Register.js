d0bf93e52b6b469ca31dbe4e14765e7e0d30eb32
Dawood149 <dawoodchaudary14@gmail.com>
2023-06-08 05:56:46 +0500

    second commit

---
+++ src/pages/Auth/Register.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import Layout from '../../components/Layouts/layout'
+
+const Register = () => {
+
+const [name, setName]=useState("")
+const [email, setEmail]=useState("")
+const [password, setPassword]=useState("")
+const [phone, setPhone]=useState("")
+const [address, setAddress]=useState("")
+
+// this function takes input data from form and print it on console.
+const handleSubmit=(e)=>{
+e.preventDefault()
+console.log(name,email,password,phone,address);
+alert("User Registered")
+}
+
+  return (
+    <Layout>
+        <div className='register'>
+        <h1 className='mb-5'>Registeration</h1>
+   <form onSubmit={handleSubmit}>
+
+  <div className="mb-3">
+    <input required placeholder='Enter your name' type="Name" className="form-control" id="exampleInputEmail1" 
+    value={name} onChange={(event)=>setName(event.target.value)}/>
+  </div>
+
+  <div className="mb-3">
+    <input required placeholder='email' type="email" className="form-control" id="exampleInputPassword1" 
+    value={email} onChange={(event)=>setEmail(event.target.value)}/>
+  </div>
+
+  <div className="mb-3">
+    <input required placeholder='Password' type="password" className="form-control" id="exampleInputPassword1" 
+    value={password} onChange={(event)=>setPassword(event.target.value)}/>
+  </div>
+
+  <div className="mb-3">
+    <input required placeholder='Phone#' type="tel" className="form-control" id="exampleInputPassword1" 
+    value={phone} onChange={(event)=>setPhone(event.target.value)}/>
+  </div>
+  
+  <div className="mb-3">
+    <input required placeholder='Address' type="address" className="form-control" id="exampleInputPassword1" 
+    value={address} onChange={(event)=>setAddress(event.target.value)}/>
+  </div>
+
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+
+
+        </div>
+    </Layout>
+   
+  )
+}
+
+export default Register
